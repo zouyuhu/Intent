17904fb1183530eed841d7d8e298a90c740325d7
zouyuhu <1470363245@qq.com>
2018-05-14 09:31:53 +0800

    Intent

---
+++ README.md
@@ -0,0 +1,8 @@
+# 自定义webview验证隐式Intent的使用
++ 本实验通过自定义WebView加载URL来验证隐式Intent的使用
++ 实验包括两个应用
+1. 第一个应用：获取URL地址并启动隐式Intent的调用。
+2. 第二个应用：自定义webview来加载URL。
++ 实验截图
+![](https://github.com/sueword/AndroidStudioProjects/blob/master/screenshot/intent1.png)
+![](https://github.com/sueword/AndroidStudioProjects/blob/master/screenshot/intent2.png)
---
+++ app/src/main/java/com/sueword/myintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sueword.myintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.m_edit);
+        button=findViewById(R.id.m_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url=editText.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                intent=Intent.createChooser(intent,"Choose Browser");
+                startActivity(intent);
+            }
+        });
+    }
+}
